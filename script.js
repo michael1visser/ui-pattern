c1b97845927b365e9dc2492c0c02b1bce0ae75da
mike_visser <mpviss@gmail.com>
2020-07-02 11:03:55 -0400

    background images and character names loading on cards

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 let characters = document.querySelectorAll(".character-card")
 
-let characterIds = [370, 216, 522, 461, 286, 576, 60, 165, 457, 538, 546, 427, 514, 558, 309, 214]
+let characterIds = [370, 216, 522, 461, 286, 576, 60, 165, 457, 538, 546, 427, 514, 558, 309, 214, 181, 678, 204, 405]
 
 let baseURL = 'https://www.superheroapi.com/api.php/10108022590356420/'
 
+let grid = document.querySelector("#character-grid")
 /* let id = 370
 console.log(`${baseURL}${id}`) */
 
-function getImage(baseURL, id){
+//let cards = document.querySelectorAll(".character-card")
 
-fetch(`${baseURL}${id}/image`)
-    .then(res => res.json())
-    .then(res =>{
-        console.log(res.url)
-        //return res.url
-        return "hi there"
-        
-    })
-    .catch(err =>{
-        console.log(`oops!: ${err}`)
-    })
-} 
+//FUNCTION TO SET CARD BACKGROUNDS AND CHARACTER NAMES
 
-let card1 = document.querySelector(".character-card")
-let image = getImage(baseURL, 427)
-console.log(image)
+function setCard(baseURL, ids){
+
+    characterIds.forEach((n,i) => {
+
+        fetch(`${baseURL}${characterIds[i]}/image`)
+            .then(res => res.json())
+            .then(res =>{
+                //console.log(res.url)
+                let card =document.createElement("div")
+                card.classList.add("character-card")
+                card.innerText = res.name
+                let image = res.url
+                console.log(image)
+                card.style.backgroundImage = `url(${image})`
+                grid.appendChild(card)
+                
+            })
+            .catch(err =>{
+                console.log(`oops!: ${err}`)
+            })
+    }) 
+}
+
+setCard(baseURL, characterIds)
+
+
+
 //let image = "https://www.superherodb.com/pictures2/portraits/10/100/731.jpg"
-card1.style.backgroundImage = `url(${image})`
+
